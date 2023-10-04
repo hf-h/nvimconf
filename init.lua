115e9dff213b8030e1e044eeb437135fe39d776f
HamsterFisk <axelburman123@gmail.com>
2023-10-04 22:25:16 +0200

    autocomplete copy paste, think I get the general idea

--- init.lua
+++ init.lua
@@ -110,12 +110,21 @@ require("lazy").setup({
 
 require("mason").setup()
 
+--Maybe add these to all lsp setups, need to figure out what it does first.
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 --LSP Setups
 local lspconfig = require("lspconfig")
 lspconfig.ols.setup {}
 lspconfig.clangd.setup {}
-lspconfig.lua_ls.setup {}
+lspconfig.lua_ls.setup {
+		capabilities = capabilities,
+}
 lspconfig.hls.setup {}
+lspconfig.pyright.setup {
+		capabilities = capabilities,
+}
 
 --LSP Key binds
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -151,6 +160,53 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+--Cmp setup yoinked from kickstart nvim
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
 --Color scheme customization
 require("tokyonight").setup({
