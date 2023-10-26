f3e72b64692b7b6507df5f17320b3860a1103219
HamsterFisk <axelburman123@gmail.com>
2023-10-26 23:34:33 +0200

    set tab key to use spaces

--- init.lua
+++ init.lua
@@ -11,13 +11,14 @@
 
 --TODO: Figure out how to highlight "TODO:" in comments
 
---local custom = require("custom")
-
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
 vim.opt.hlsearch = true
 
 vim.g.mapleader = ' '
